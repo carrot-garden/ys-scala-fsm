5a499668d2fe46ba09051424e3a866313f3c2acd
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-11-13 10:00:59 +0200

    Input

--- src/main/scala/com/stulsoft/fsm/Input.scala
+++ src/main/scala/com/stulsoft/fsm/Input.scala
@@ -1,6 +1,10 @@
 package com.stulsoft.fsm
 
-/**
+/** Input class
+  *
+  * @param name       input name
+  * @param parameters map with parameters (parameter name -> parameter value)
+  * @tparam T parameter type
   * @author Yuriy Stul.
   */
-case class Input[T:Parameter](name: String, parameters: Map[String, T])
+case class Input[T >: Parameter[Any]](name: String, parameters: Map[String, T])
--- src/test/scala/com/stulsoft/fsm/InputTest.scala
+++ src/test/scala/com/stulsoft/fsm/InputTest.scala
@@ -2,13 +2,18 @@ package com.stulsoft.fsm
 
 import org.scalatest.{FlatSpec, Matchers}
 
-/**
+/** Unit tests for Input class
+  *
   * @author Yuriy Stul.
   */
 class InputTest extends FlatSpec with Matchers {
   behavior of "ParameterText"
   "constructor" should "support different type of parameters" in {
-   val i = Input("test input", Map("p1"->TextParameter("abc"), "p2"->IntParameter(1)))
-    i should exist
+    val inputName = "test input"
+    val i = Input(inputName, Map("p1" -> TextParameter("abc"), "p2" -> IntParameter(1)))
+    i.name shouldBe inputName
+    i.parameters("p1") match {
+      case TextParameter(x) => x == "abc" shouldBe true
+    }
   }
 }
