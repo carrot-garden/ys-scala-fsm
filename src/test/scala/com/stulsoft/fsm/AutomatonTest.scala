4a9d81dc1d0f271a0c7104a4a1ab5aa9e6d3999e
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-11-14 15:17:43 +0200

    unit test

--- src/test/scala/com/stulsoft/fsm/AutomatonTest.scala
+++ src/test/scala/com/stulsoft/fsm/AutomatonTest.scala
@@ -41,7 +41,51 @@ class AutomatonTest extends FlatSpec with Matchers {
     val nextState = Automaton.nextState(currentState, input1, transitions)
     nextState match {
       case Some(_) =>
-        fail("next state should be NOT exist")
+        fail("next state should NOT exist")
+      case None =>
+        succeed
+    }
+  }
+  it should "work with aggregation - success" in {
+    val currentState: State = State("start")
+    val expectedState: State = State("finish")
+    val input1 = Input("input 1", Map("p1" -> IntParameter(1), "p2" -> TextParameter("test")))
+    val conditions = List(
+      TransitionCondition(paramName = "p1", compareType = CompareType.Equal, expectedValue = IntParameter(1)),
+      TransitionCondition(paramName = "p2", compareType = CompareType.Equal, expectedValue = TextParameter("test"))
+    )
+    val transitions = Vector(
+      Transition(input = input1,
+        priority = 100,
+        sourceState = State("start"),
+        destinationState = State("finish"),
+        conditions = conditions,
+        aggregationType = ConditionAggregationType.All))
+    val nextState = Automaton.nextState(currentState, input1, transitions)
+    nextState match {
+      case Some(newState) =>
+        newState shouldBe expectedState
+      case None => fail("next state should be exist")
+    }
+  }
+  it should "work with aggregation - failure" in {
+    val currentState: State = State("start")
+    val input1 = Input("input 1", Map("p1" -> IntParameter(1), "p2" -> TextParameter("testERROR")))
+    val conditions = List(
+      TransitionCondition(paramName = "p1", compareType = CompareType.Equal, expectedValue = IntParameter(1)),
+      TransitionCondition(paramName = "p2", compareType = CompareType.Equal, expectedValue = TextParameter("test"))
+    )
+    val transitions = Vector(
+      Transition(input = input1,
+        priority = 100,
+        sourceState = State("start"),
+        destinationState = State("finish"),
+        conditions = conditions,
+        aggregationType = ConditionAggregationType.All))
+    val nextState = Automaton.nextState(currentState, input1, transitions)
+    nextState match {
+      case Some(_) =>
+        fail("next state should NOT exist")
       case None =>
         succeed
     }
